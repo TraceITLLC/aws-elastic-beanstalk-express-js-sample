8de9a79506a5400a37ae93930e5bfab853ee449e
Dev-Innovate <77415806+Dev-Innovate@users.noreply.github.com>
2021-02-17 12:32:37 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is my first js script and webapp'));
+app.get('/', (req, res) => res.send('This is my first js script and webapp with CodePipeline and CodeCommit'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
